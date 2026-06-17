7d4d376e2b4985320ea23e131b1e7e879c96a156
David <106400065+elldav@users.noreply.github.com>
2026-06-17 17:13:32 -0500

    More pinning of the versions.

--- infra/providers.tf
+++ infra/providers.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "3.117.1"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0"
+      version = "4.3.0"
     }
   }
 }
